358d8c55946d5c443b1ed564d02990752ceb1bc3
felipe tavares <felltsp@gmail.com>
2013-11-25 17:59:48 -0200

    push

---
+++ src/com/felipetavares/photomarked/ConfigurationActivity.java
@@ -0,0 +1,14 @@
+package com.felipetavares.photomarked;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfigurationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_configuration);
+	}
+
+}
