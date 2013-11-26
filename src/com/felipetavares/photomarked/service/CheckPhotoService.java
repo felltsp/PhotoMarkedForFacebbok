b748cf0cbcca73c116627ceb8978e78ddc6459c4
felipe tavares <felltsp@gmail.com>
2013-11-26 11:30:24 -0200

    implementação de teste do serviço

--- src/com/felipetavares/photomarked/service/CheckPhotoService.java
+++ src/com/felipetavares/photomarked/service/CheckPhotoService.java
@@ -3,6 +3,7 @@ package com.felipetavares.photomarked.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 /**
  * Class com o objtivo de verificar se exitem novas fotos marcadas disponiveis para se fazer o download.
@@ -18,4 +19,22 @@ public class CheckPhotoService extends Service {
 	public IBinder onBind(Intent intent) {
 		throw new UnsupportedOperationException("Not yet implemented");
 	}
+	
+	 @Override
+     public void onCreate() {
+            Toast.makeText(getApplicationContext(), "Service Created", Toast.LENGTH_LONG).show();
+            super.onCreate();
+     }
+
+     @Override
+     public void onDestroy() {
+            Toast.makeText(getApplicationContext(), "Service Destroy", Toast.LENGTH_LONG).show();
+            super.onDestroy();
+     }
+
+     @Override
+     public int onStartCommand(Intent intent, int flags, int startId) {
+            Toast.makeText(getApplicationContext(), "Service Running ", Toast.LENGTH_LONG).show();
+            return super.onStartCommand(intent, flags, startId);
+     }
 }
